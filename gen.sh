f7b411220a87faadb83d18c8d45901c409f33553
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-09-10 20:43:23 +0200

    gen.sh modified

--- gen.sh
+++ gen.sh
@@ -5,40 +5,68 @@ set -e
 cd "$(dirname -- "$0")"
 
 C=buster
+T=buster
+V=minbase
+K=debian-archive-keyring.gpg
+R=http://192.168.93.8:3142/deb.debian.org/debian/
+I=vim
+
+U="$(id -u)"
+G="$(id -g)"
+
+L="$(readlink -e -- LXC)"
+printf -vD '%s.%(%Y%m%d-%H%M%S)T' "$L/$C" -1
+
+P="$D/trust.d/"
+printf -vA '"%q"' "$P"
+printf -vX '%06x' "$(stat -tLc %i "$D")"
+
+mkdir "$D" "$P"
+for a in /usr/share/keyrings
+do
+	[ -s "$a/$K" ] && ln -vs --backup=t --relative "$a/$K" "$P"
+done
 
 ARGS=()
-ARGS+=(--aptopt="Dir::Etc::TrustedParts \"$PWD/debian.trusted/\";")
-ARGS+=(-v --debug)
-ARGS+=(--variant=minbase)
-ARGS+=(--include=ifupdown,systemd-sysv,vim)
-ARGS+=(buster)
-ARGS+=("lxc/$C/rootfs")
-ARGS+=(http://192.168.93.8:3142/deb.debian.org/debian/)
+ARGS+=(-v)
+ARGS+=(--debug)
+ARGS+=("--variant=$V")
+ARGS+=("--include=ifupdown,systemd-sysv,$I")
+ARGS+=("--aptopt=Dir::Etc::TrustedParts $A;")
+ARGS+=("$T")
+ARGS+=("$D/rootfs")
+ARGS+=("$R")
 
-LXC="$(readlink -e -- LXC)"
-
-mkdir "lxc/$C"
 mmdebstrap "${ARGS[@]}"
 
-cat <<EOF >"lxc/$C/config/$C.config"
-
+{
+cat <<EOF
 # Distribution configuration
 lxc.include = /usr/share/lxc/config/common.conf
 lxc.include = /usr/share/lxc/config/userns.conf
 lxc.arch = linux64
-
-# Container specific configuration
-lxc.idmap = u 0 `id -u` 1
-lxc.idmap = g 0 `id -g` 1
-lxc.idmap = u 1 100000 65536
-lxc.idmap = g 1 100000 65536
-lxc.rootfs.path = dir:$(readlink -e/home/tino/.local/share/lxc/buster/rootfs
-lxc.uts.name = buster
-
-# Network configuration
-lxc.net.0.type = veth
-lxc.net.0.link = lxcbr0
-lxc.net.0.flags = up
-lxc.net.0.hwaddr = 00:16:3e:54:a3:0d
+lxc.rootfs.path = dir:$D/rootfs
+lxc.uts.name = $C
 EOF
+
+## Container specific configuration
+#lxc.idmap = u 0 $U 1
+#lxc.idmap = g 0 $G 1
+#lxc.idmap = u 1 100000 65536
+#lxc.idmap = g 1 100000 65536
+#
+## Network configuration
+#lxc.net.0.type = veth
+#lxc.net.0.link = lxcbr0
+#lxc.net.0.flags = up
+# 00163e is Xensource, Inc.:
+#lxc.net.0.hwaddr = 00:16:3e:xx:xx:xx`
+
+awk -vX="$X" '
+/:xx/	{ while (/:xx/) { l=length(X); x=substr(X,l-1); X=substr(X,0,l-2); if (x=="") x="00"; sub(/:xx/,x) } }
+			{print}
+' $HOME/.config/lxc/default.conf
+} >"$D/config"
+
+printf -- '--------------------------------\n%q\n--------------------------------\n' "$D"
 
