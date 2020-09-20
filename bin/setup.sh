9a2fdf98f66bdd849f15932be0ff42e3ba50c462
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-09-20 08:21:51 +0200

    bin/setup.sh modified

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #
-#U Usage: {ARG0}
-#U	LXC setup
+#U Usage:
+#U	Unprivileged LinuX Containers initial setup
+#U	Other commands implicitely invoke this if needed.
+#U
 #U	In future you might be able to change some parameters interactively
-#U	For now this here is fully automatic.
-#U	For now it only works from scratch.
-#U	In that case, just do, what it says, to get it up and running.
-#U	But, as always, USE AT OWN RISK!
+#U	- For now this here is fully automatic.
+#U	- For now it only works from scratch.
+#U	- In that case, just do, what it says, to get it up and running.
+#U
+#U	As always, think first, as everything you do is AT YOUR OWN RISK!
+#
+# This Works is placed under the terms of the Copyright Less License,
+# see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
 
 ME="$(readlink -e -- "$0")" || exit
 . "${ME%/*/*}/lxc-inc/lxc.inc" || exit
