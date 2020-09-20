f59806c552480b920c64f53ff4cbd979383fd58b
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-09-20 08:21:51 +0200

    bin/lxc.sh modified

--- bin/lxc.sh
+++ bin/lxc.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #
-#U Usage: {ARG0} CMD ARGS..
-#U	This runs {PATH}/CMD.sh ARGS..
-#U	where:
-#U	CMD is a container command
-#U	ARGS.. are additional arguments to the command
-#U	For all possible commands run: {ARG0} help
+#U Usage: CMD ARGS..
+#U	Run container command {PATH}/bin/CMD.sh
+#U	Example: {ARG0} help
+#
+# This Works is placed under the terms of the Copyright Less License,
+# see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
 
 ME="$(readlink -e -- "$0")" || exit
 . "${ME%/*/*}/lxc-inc/lxc.inc" || exit
 
 [ 0 = $# ] && Usage
 o check-name "$1" invalid command name
-[ -x "$BIN/$1.sh" ] || OOPS unknown command: "$1"
+[ -x "$BASE/bin/$1.sh" ] || OOPS unknown command: "$1"
 
-LXC_USAGE="${ME##*/} $1" exec "$BIN/$1.sh" "${@:2}"
+LXC_USAGE="${0##*/} $1" exec "$BASE/bin/$1.sh" "${@:2}"
 
