c733848f18db1ddb3b463e5d188e48e604c582a9
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-09-20 08:21:51 +0200

    bin/run.sh modified

--- bin/run.sh
+++ bin/run.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 #
-# Run a command as the container's LXC_UID and LXC_GID
-# run.sh container command args..
+#U Usage: CONTAINER CMD ARGS..
+#U	Run CMD with ARGS within CONTAINER
+#U	Options are passed via environment.
+#U	This returns the return value of CMD
+#U
+#U	uid/gid defaults to your mapped user in the container
+#U	if not overridden with LXC_UID/LXC_GID.
+#U
+#U	Environment variables which start with the prefix RUN_
+#U	or are listed in the comma or blank separated list LXC_ENV
+#U	are available inside the container.  LXC_ENV='*' for all.
+#U
+#U	Example:
+#U		LXC_UID=1 LXC_GID=1 {CMD} CONTAINER id
 #
-# There are no options:
-# - Drop environment
-# - Allow all variables LXC_xxx
-# - Allow all variables in LXC_ENV
+# This Works is placed under the terms of the Copyright Less License,
+# see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
 
 ARGS=()
 ARGS+=(--clear-env)
