e1905547374b43ee0eef6c1d07b6687ff9259328
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-09-21 14:02:35 +0200

    fixup! bin/setup.sh modified

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #
-#U Usage:
+#U Usage: [NAME]
 #U	Unprivileged LinuX Containers initial setup
 #U	Other commands implicitely invoke this if needed.
+#U
+#U	If NAME is given this creates a wrapper in $HOME/bin/NAME to LXC
 #U
 #U	In future you might be able to change some parameters interactively
 #U	- For now this here is fully automatic.
@@ -16,6 +18,51 @@
 
 ME="$(readlink -e -- "$0")" || exit
 . "${ME%/*/*}/lxc-inc/lxc.inc" || exit
+
+[ 1 -ge $# ] || Usage
+
+note()
+{
+  local a
+  {
+	printf 'Cannot detect wrapper\n'
+	printf 'for %q\n' "$CMP"
+	ov a readlink -e "$HOME/bin/."
+	printf 'in  %q\n' "$a"
+	for a
+	do
+		printf '%s\n' "$a"
+	done
+  } | squelch wrapper
+}
+
+check()
+{
+  ov CMP readlink -e "$BASE/bin/lxc.sh"
+  for a in "$HOME"/bin/*
+  do
+	[ -L "$a" ] || continue
+	ov CHK readlink -e "$a"
+	[ ".$CMP" = ".$CHK" ] && return 0
+  done
+
+  ov a readlink -m "$HOME/bin/LXC"
+  if	[ -L "$a" ] || [ -e "$a" ]
+  then
+	note 'Please re-invoke setup with a proper name as first argument.' '(Name LXC already taken.)'
+  else
+	note "Creating default wrapper $a"
+	return 1
+  fi
+}
+
+setup()
+{
+  o check-name "$1"
+  ov a readlink -m "$HOME/bin/$1"
+}
+
+[ -z "$1" ] && check || setup "${1:-LXC}"
 
 STDERR note: It is likely that setup is still missing some bits.
 EXIT
