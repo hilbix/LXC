393e4859475794fd3f3b7d20e6d858d9ea48cbda
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-09-21 13:59:13 +0200

    bin/create.sh modified

--- bin/create.sh
+++ bin/create.sh
@@ -13,38 +13,23 @@
 #U	- can be overwritten in the environment.
 #U	To list the parameters use 'diag' command
 #
-#	# Default: root.  In Linux the first user usually is 1000/1000
-#	LXC_UID=0
-#	LXC_GID=0
-#	# Parameters to mmdebstrap, see man mmdebstrap
-#	LXC_SUITE=buster
-#	LXC_VARIANT=minbase
-#	LXC_MIRROR=http://deb.debian.org/debian/
-#	# Additional comma separated parameters to mmdebstrap:
-#	## Keys to use for apt-get to check the signatures
-#	LXC_KEYS=debian-archive-keyring.gpg
-#	## List of additional packages to install
-#	LXC_INCLUDE=vim
-#
 # This Works is placed under the terms of the Copyright Less License,
 # see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
 
+LXC_ARGS=1-2
 ME="$(readlink -e -- "$0")" || exit
 . "${ME%/*/*}/lxc-inc/lxc.inc" || exit
 
 # get commandline args, else present usage
-CONTAINER="$1"
-SETTINGS="${2:-$1}"
-
-[ -n "$SETTINGS" ] || Usage
-
+o LXCcontainer "$1"
+o LXCtemplate "${2:-$1}"
 
 # Do the install
 # (this perhaps is not elaborate enough yet and might change/improve in future)
-o settings-read-or-create "$BASE/CONF/lxc-$CONTAINER.config"
-o lxc-container-config "$BASE/CONF/lxc-$CONTAINER.config"
-o settings-print GET
-o lxc-container-mmdebstrap "$BASE/LXC/$CONTAINER/rootfs"
+o LXCsettings-create
+o LXCcontainer-setup
+o settings-print
+o LXCcontainer-mmdebstrap
 
-EXIT
+LXCexit
 
