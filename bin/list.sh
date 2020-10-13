0aae44bb314eedea1570ae82856deec4cadde2d4
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-10-13 18:02:13 +0200

    bin/list.sh modified

--- bin/list.sh
+++ bin/list.sh
@@ -6,15 +6,28 @@
 # This Works is placed under the terms of the Copyright Less License,
 # see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
 
+LXC_ARGS=0
 ME="$(readlink -e -- "$0")" || exit
 . "${ME%/*/*}/lxc-inc/lxc.inc" || exit
 
-for a in "$BASE"/LXC/*/config
+len=0
+for a in "$LXC_BASE"/LXC/*/config
 do
 	[ -s "$a" ] || continue
 	b="${a%/config}"
 	b="${b##*/}"
-	echo "$b"
+	state="$(lxc-info -n "$b" -Hs)"
+	ip="$(lxc-info -n "$b" -Hi)"
+	printf -vc "name=%q state=%q ip=%q" "$b" "$state" "$ip"
+	MACH+=("$c")
+	[ "$len" -lt "${#b}" ] && len="${#b}"
 done
-exit $MURX
+
+for a in "${MACH[@]}"
+do
+	eval "$a"
+	printf "%*q %q %q\n" "$len" "$name" "$state" "$ip"
+done
+
+LXCexit
 
