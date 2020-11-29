29302c37e1c11ccca9e1e56d2278cb646e053305
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-11-29 18:03:41 +0100

    bin/run.sh corrected
    RUN_* environment variables were not passed

--- bin/run.sh
+++ bin/run.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 #
 #U Usage: CONTAINER CMD ARGS..
-#U	Run CMD with ARGS within CONTAINER
+#U	Run CMD with ARGS as root within CONTAINER
 #U	Options are passed via environment.
-#U	This returns the return value of CMD
-#U
-#U	uid/gid defaults to your mapped user in the container
-#U	if not overridden with LXC_UID/LXC_GID.
+#U	This returns the return value of CMD.
+#U	If fails if CONTAINER is not started.
 #U
 #U	Environment variables which start with the prefix RUN_
 #U	or are listed in the comma or blank separated list LXC_ENV
 #U	are available inside the container.  LXC_ENV='*' for all.
 #U
-#U	Example:
-#U		LXC_UID=1 LXC_GID=1 {CMD} CONTAINER id
+#U	Example to change owner of /tmp/file to your user:
+#U		LXC_UID=1 LXC_GID=1 {CMD} CONTAINER chown 1:1 /tmp/file
+#U	This examples DOES NOT WORK yet, as LXC_UID is not yet supported!
 #
 # This Works is placed under the terms of the Copyright Less License,
 # see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
@@ -29,7 +28,7 @@ ARGS=()
 if [ '.*' != ".$LXC_ENV" ]
 then
 	ARGS+=(--clear-env)
-	for a in ${LXC_ENV//,/ }
+	for a in ${!RUN_*} ${LXC_ENV//,/ }
 	do
 		ARGS+=(--keep-var "$a")
 	done
